9c241dabe608bce1f318f1abb96a056250600907
Andrei2348 <makarevichandco@gmail.com>
2024-02-24 12:07:40 +0300

    начало пробного проекта

--- src/components/users.js
+++ src/components/users.js
@@ -29,10 +29,13 @@ class Users extends React.Component {
                     <div className='user' key={el.id}>
                         <h3>{el.firstname} {el.lastname}</h3>
                         <p>{el.bio}</p>
+                        <b>{el.isHappy ? 'Счастлив :)' : 'Не особо :('}</b>
                     </div>
                 ))}
             </div>
             )
+            else
+            return (<div className="user"><h3>Пользователей нет</h3></div>)
     }
   }
 
